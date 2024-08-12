dd2da2fbb4e0d3312614a2f525c84d2addeb535a
Dhruv-Bagora <https://github.com/Dhruv-Bagora>
2024-08-12 12:01:43 +0530

    One more testing

--- var.tf
+++ var.tf
@@ -88,6 +88,6 @@ variable "instance_tags_2" {
   description = "Tags to apply to the EC2 instance"
   type        = map(string)
   default = {
-    Name = "Jenkins_public_instance_test"
+    Name = "Jenkins_public_instance_test_oncemore"
   }
 }
